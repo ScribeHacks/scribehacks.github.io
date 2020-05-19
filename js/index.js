dfe07d26364ab556fa06671eedd64e202bd30d2b
Rohan Bansal <bansalrohan18@gmail.com>
2020-05-19 12:56:04 -0700

    bug fixes, removed placeholder

--- js/index.js
+++ js/index.js
@@ -9,3 +9,13 @@ new fullpage('#fullpage', {
     easing: 'easeInOutCubic',
 	easingcss3: 'ease',
 });
+
+function initTimeTable() {
+    var timetable = new Timetable();
+    timetable.setScope(9, 23);
+    timetable.useTwelveHour();
+    timetable.addLocations(['Silent Disco', 'Nile', 'Len Room', 'Maas Room']);
+
+    var renderer = new Timetable.Renderer(timetable);
+    renderer.draw('.timetable');
+}
