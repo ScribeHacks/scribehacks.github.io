f2cdb375c59071514cf7b22d41668231f9b884a1
Rohan Bansal <bansalrohan18@gmail.com>
2020-05-18 21:27:32 -0700

    scroll page fixes, ui/typewriter

---
+++ js/index.js
@@ -0,0 +1,13 @@
+new fullpage('#fullpage', {
+    navigation: true,
+    navigationPosition: 'right',
+    scrollHorizontally: true,
+    controlArrows: false,
+    scrollOverflow: true,
+
+    slidesNavigation: true,
+	slidesNavPosition: 'bottom',
+
+    easing: 'easeInOutCubic',
+	easingcss3: 'ease',
+});
--- js/typewriter.js
+++ js/typewriter.js
@@ -3,13 +3,14 @@ function initTypeEffect() {
     new TypeIt('#info-text', {
         waitUntilVisible: true
     })
-    .type(".date(April 17-18);")
+    .pause(500)
+    .type('.date("April 17-18");')
     .pause(2000)
     .delete(30)
-    .type(".registerBy(April 15);")
+    .type('.registerBy("April 15");')
     .pause(2000)
     .delete(30)
-    .type(".location(2727 Hackathon Street, San Jose, CA);")
+    .type('.location("2727 Hackathon Street, San Jose, CA");')
     .pause(2000)
     .delete(50)
     .options({lifeLike: true, loop: true})
