a58fbe42f758059da71d25d4737b3235eb31023c
Soham Bhattacharya <sohamb117@gmail.com>
2020-10-10 14:26:12 -0700

    Styled things

--- js/index.js
+++ js/index.js
@@ -127,7 +127,8 @@ for (i = 0; i < acc.length; i++) {
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      var panelheight = panel.scrollHeight + 10;
+      panel.style.maxHeight = panelheight + "px";
     } 
   });
 }
