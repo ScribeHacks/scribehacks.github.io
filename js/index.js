8c76e21d62f57606a1830bdc577560a47cbdae0a
Rohan Bansal <bansalrohan18@gmail.com>
2020-10-09 19:16:31 -0700

    fix faq

--- js/index.js
+++ js/index.js
@@ -133,3 +133,19 @@ for (i = 0; i < acc.length; i++) {
 }
 
 // TODO: Write neocordion EdgeHTML polyfill
+
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
