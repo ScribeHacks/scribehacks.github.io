a27e177f0432cea14b27ef9b3043993e491e0801
Kai Devrim <kkgithub@devrim.tech>
2020-11-27 12:00:47 -0800

    finished the front page

--- mailer.php
+++ mailer.php
@@ -22,7 +22,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "contact@markups.io";
+        $recipient = "contact@scribehacks.tech";
 
         // Set the email subject.
         $subject = "New contact from $name";
