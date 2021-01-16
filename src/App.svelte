655e5119564985599df96c25565d231d54601c26
Kyle <sdbagel@gmail.com>
2021-01-16 01:19:55 -0800

    component: Fixed site navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+  import Navbar from "./components/Navbar.svelte";
   import Hero from "./sections/Hero.svelte";
 </script>
 
 <main>
+  <Navbar />
   <Hero />
 </main>
 
 <style type="text/scss">
-
 </style>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,36 @@
+
+<nav class="nav">
+  <a class="nav-link" href="#about">About</a>
+  <a class="nav-link" href="#schedule">Schedule</a>
+  <a class="nav-link" href="#map">Map</a>
+  <a class="nav-link" href="#faq">FAQ</a>
+  <a class="nav-link" href="#team">Team</a>
+  <a class="nav-link" href="#contact">Contact</a>
+</nav>
+
+<style type="text/scss">
+  .nav {
+    background-color: #fff;
+    border-radius: 30px;
+    display: flex;
+
+    padding: 0 25px;
+    position: fixed;
+    top: 30px;
+    right: 30px;
+    
+    overflow: hidden;
+  }
+
+  .nav-link {
+    color: #f00;
+    text-decoration: none;
+    padding: 20px;
+    transition-duration: 0.2s;
+
+    &:hover {
+      background-color: #f50;
+      color: #fff;
+    }
+  }
+</style>
