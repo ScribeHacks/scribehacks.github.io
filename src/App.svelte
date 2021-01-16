3e4e7e847bb9bc8a782a394ba5d188d00fc48ad2
Kyle <sdbagel@gmail.com>
2021-01-16 01:58:22 -0800

    section: Add redesigned about section

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import Navbar from "./components/Navbar.svelte";
   import Hero from "./sections/Hero.svelte";
+  import About from "./sections/About.svelte";
 </script>
 
 <main>
   <Navbar />
   <Hero />
+  <About />
 </main>
 
 <style type="text/scss">
---
+++ src/sections/About.svelte
@@ -0,0 +1,50 @@
+
+<section class="section about">
+  <div class="section-left">
+    <h2>Write the code of the future.</h2>
+  </div>
+  <div class="section-right">
+    <p><b>Coding gives us the power to make things.</b><br>
+    Every Facebook, Google, and Microsoft started with just a bit of code, so we want to encourage each person to learn to make something impactful or through technology.</p>
+  </div>
+</section>
+
+<style type="text/scss">
+  .about {
+    display: flex;
+    align-items: center;
+
+    .section-left, .section-right {
+      flex: 1 1;
+      padding: 30px 60px;
+    }
+    
+    .section-left {
+      display: flex;
+      justify-content: flex-end;
+    }
+
+    .section-right {
+      background-color: #333;
+      display: flex;
+      justify-content: flex-start;
+    }
+
+    h2, p {
+      max-width: 400px;
+    }
+
+    h2 {
+      font-size: 3.3rem;
+    }
+
+    p {
+      background-color: #333;
+      border-radius: 5px;
+      color: #eee;
+      font-size: 1.2rem;
+      font-weight: 300;
+      line-height: 3rem;
+    }
+  }
+</style>
